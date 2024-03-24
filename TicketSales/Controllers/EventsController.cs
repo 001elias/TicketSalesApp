8b6f5f1915e3d9793be192efce1d9092115a161c
eli <ebarrellet@gmail.com>
2024-03-23 20:41:42 -0400

    Started Details Page

--- TicketSales/Controllers/EventsController.cs
+++ TicketSales/Controllers/EventsController.cs
@@ -1,17 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using Microsoft.AspNetCore.Mvc; // Include this for EF Core functionality
+using System.Linq;
+using TicketSales.Models;
+using System.Data.Entity;
 namespace TicketSales.Controllers
 {
     public class EventsController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        private readonly TicketReservationContext db = new TicketReservationContext(); // Assuming your DbContext
 
-        public IActionResult Details(int eventId)
+        public async Task<ActionResult> Details(int eventId)
         {
-            return View();
+            // Fetch the event including its Venue using EF6
+            var evt = await db.Events.Include("Venue")
+                                     .SingleOrDefaultAsync(e => e.EventId == eventId);
+
+            if (evt == null)
+            {
+                return NotFound();
+            }
+
+            return View(evt);
         }
     }
 }
--- TicketSales/obj/Debug/net8.0/TicketSales.AssemblyInfo.cs
+++ TicketSales/obj/Debug/net8.0/TicketSales.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketSales")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+181c90556929e20b2ff9c44530c472c88bef08a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38221f8e6d06ed51137c196862c92bec9164263b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketSales")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketSales")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
